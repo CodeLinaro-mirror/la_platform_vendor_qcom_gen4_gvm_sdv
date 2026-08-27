cbdb6a5a26b23dd39eeb5ac3e4c3fc7ae93f84f1
Swapneel Mashalkar <smashalk@qti.qualcomm.com>
2026-08-26 22:49:28 -0700

    [sdv]: Revert back the default console to virtio_console
    Switching from pl011 to virtio_console as default.
    
    Change-Id: Id62692b3246f7b81f43b2e16f08fa168f7a5092e
    Signed-off-by: Swapneel Mashalkar <smashalk@qti.qualcomm.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,10 +4,10 @@ TARGET_2ND_ARCH := arm
 
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 10737418240
 BOARD_BOOTCONFIG := androidboot.hardware=qcom androidboot.selinux=permissive androidboot.memcg=1 androidboot.recover_usb=1
-BOARD_KERNEL_CMDLINE := user_debug=31 print-fatal-signals=1  init=/init swiotlb=4096  kpti=0 pcie_ports=compat firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 debug loglevel=9
-BOARD_BOOTCONFIG += androidboot.console=ttyAMA0
-BOARD_KERNEL_CMDLINE += printk.devkmsg=on log_buf_len=10M  printk_ratelimit=0 printk_ratelimit_burst=0 debug loglevel=9
-BOARD_KERNEL_CMDLINE += earlycon=pl011,0x1c090000 console=ttyAMA0
+BOARD_KERNEL_CMDLINE := user_debug=31 print-fatal-signals=1  init=/init swiotlb=4096 kpti=0 pcie_ports=compat firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 debug loglevel=9
+
+BOARD_KERNEL_CMDLINE += printk.devkmsg=on printk_ratelimit=0 printk_ratelimit_burst=0
+
 BOARD_BOOTCONFIG += androidboot.sdv.instance_name=instance1 androidboot.sdv.boot_mode=unlocked androidboot.sdv.ignore_avb_state=true androidboot.sdv.vvmfactorytrust=c779a73d0595a6814ba0414a419e99ad4026ad0feb603f2ad80ee6a9e4d1adb7 androidboot.sdv.keymint.rpc.hbk=799da7577efd41d5b27810c5952fcec0291cbcfd687e77ac9a6cec8370651b1d
 BOARD_BOOTCONFIG += androidboot.init_rc=$(LOCAL_ANDROIDBOOT_INIT_RC) \
                     kernel.vmw_vsock_virtio_transport_common.virtio_transport_max_vsock_pkt_buf_size=16384 \
@@ -17,8 +17,7 @@ BOARD_BOOTCONFIG += androidboot.init_rc=$(LOCAL_ANDROIDBOOT_INIT_RC) \
 
 BOARD_KERNEL_CMDLINE +=  log_buf_len=4M \
                          audit=1 \
-                         panic=-1 \
-                         init_rc=$(LOCAL_ANDROIDBOOT_INIT_RC)
+                         panic=-1
 
 TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
 TARGET_USERIMAGES_SPARSE_F2FS_DISABLED := false
